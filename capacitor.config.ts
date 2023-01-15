cdbeea4b87f84efe0f27c7223cf6b820fb0ca24b
dadiallo <d.abdoulayead28@gmail.com>
2023-01-15 20:27:49 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'odc-interview-mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
